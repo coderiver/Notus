79f43908ad6ead6a6b37714c5be33603a28f710b
mirantel <magi.mirantel@gmail.com>
2012-09-24 17:23:35 +0300

    test

--- config.rb
+++ config.rb
@@ -1,11 +1,13 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+
+http_path = ""
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
+relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
